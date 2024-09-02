1f79cc7507eccb80fb43f1e4730c744543cfba24
dllano <dllano@ucb.edu.bo>
2024-09-02 07:04:19 -0700

    Added EF Postgresql, Model User and DB Context

---
+++ Models/PgContext.cs
@@ -0,0 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+
+public class PgDbContext : DbContext
+{
+    public PgDbContext(DbContextOptions<PgDbContext> options) : base(options) { }
+
+    public DbSet<User> Users { get; set; }
+}
