ebb723958cdb547d340e7af8dac5afdcd3fb4b67
Paolo Velarde <71466134+PaoloVelarde7544@users.noreply.github.com>
2024-09-09 21:46:58 -0400

    Revert "Created RolesController in Controllers, Roles in Models and added scr…"

--- Models/PgContext.cs
+++ Models/PgContext.cs
@@ -5,7 +5,4 @@ public class PgDbContext : DbContext
     public PgDbContext(DbContextOptions<PgDbContext> options) : base(options) { }
 
     public DbSet<User> Users { get; set; }
-
-    public DbSet<Role> Roles { get; set; }
-
 }
--- Models/Role.cs
+++
@@ -1,8 +0,0 @@
-public class Role
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Status { get; set; }
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
-}
